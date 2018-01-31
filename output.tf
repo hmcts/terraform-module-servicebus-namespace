d1706df4864274b3af1092cf2f175321e6aa477a
leszekg <gonczar@gmail.com>
2018-01-31 10:51:10 +0000

    Change the module to use Azure Resource Manager template
    The out-of-the-box resource from Hashicorp doesn't support shared access keys and many other things.

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
-output "name" {
-  value = "${azurerm_servicebus_namespace.namespace.name}"
+# primary connection string for send and listen operations
+output "primary_send_and_listen_connection_string" {
+  value = "${azurerm_template_deployment.namespace.outputs["primaryConnectionStringForSendAndListen"]}"
+}
+
+# secondary connection string for send and listen operations
+output "secondary_send_and_listen_connection_string" {
+  value = "${azurerm_template_deployment.namespace.outputs["secondaryConnectionStringForSendAndListen"]}"
+}
+
+# primary shared access key with send and listen rights
+output "primary_send_and_listen_shared_access_key" {
+  value = "${azurerm_template_deployment.namespace.outputs["primarySharedAccessKeyForSendAndListen"]}"
+}
+
+# secondary shared access key with send and listen rights
+output "secondary_send_and_listen_shared_access_key" {
+  value = "${azurerm_template_deployment.namespace.outputs["secondarySharedAccessKeyForSendAndListen"]}"
 }
