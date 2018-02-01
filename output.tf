ab0cb3fd2653501c51d648fcc02c65062f9dadce
leszekg <gonczar@gmail.com>
2018-02-01 16:09:05 +0000

    Make the names of connection strings and shared access keys consistent

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 # primary connection string for send and listen operations
 output "primary_send_and_listen_connection_string" {
-  value = "${azurerm_template_deployment.namespace.outputs["primaryConnectionStringForSendAndListen"]}"
+  value = "${azurerm_template_deployment.namespace.outputs["primarySendAndListenConnectionString"]}"
 }
 
 # secondary connection string for send and listen operations
 output "secondary_send_and_listen_connection_string" {
-  value = "${azurerm_template_deployment.namespace.outputs["secondaryConnectionStringForSendAndListen"]}"
+  value = "${azurerm_template_deployment.namespace.outputs["secondarySendAndListenConnectionString"]}"
 }
 
 # primary shared access key with send and listen rights
 output "primary_send_and_listen_shared_access_key" {
-  value = "${azurerm_template_deployment.namespace.outputs["primarySharedAccessKeyForSendAndListen"]}"
+  value = "${azurerm_template_deployment.namespace.outputs["primarySendAndListenSharedAccessKey"]}"
 }
 
 # secondary shared access key with send and listen rights
 output "secondary_send_and_listen_shared_access_key" {
-  value = "${azurerm_template_deployment.namespace.outputs["secondarySharedAccessKeyForSendAndListen"]}"
+  value = "${azurerm_template_deployment.namespace.outputs["secondarySendAndListenSharedAccessKey"]}"
 }
