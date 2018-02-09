32eeff220bcb1aa5449cca83779eec42a16a1a60
leszekg <gonczar@gmail.com>
2018-02-09 15:27:28 +0000

    Add namespace name to output variables

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "name" {
+  value = "${azurerm_template_deployment.namespace.name}"
+}
+
 # primary connection string for send and listen operations
 output "primary_send_and_listen_connection_string" {
   value = "${azurerm_template_deployment.namespace.outputs["primarySendAndListenConnectionString"]}"
