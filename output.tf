5f7561526b88b9345844ba7ace3609d860f8f3d2
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-02-17 18:08:10 +0000

    Bug fix

--- output.tf
+++ output.tf
@@ -4,22 +4,22 @@ output "name" {
 
 # primary connection string for send and listen operations
 output "primary_send_and_listen_connection_string" {
-  value = azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule.primary_connection_string
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.primary_connection_string
 }
 
 # secondary connection string for send and listen operations
 output "secondary_send_and_listen_connection_string" {
-  value = azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule.secondary_connection_string
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.secondary_connection_string
 }
 
 # primary shared access key with send and listen rights
 output "primary_send_and_listen_shared_access_key" {
-  value = azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule.primary_key
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.primary_key
 }
 
 # secondary shared access key with send and listen rights
 output "secondary_send_and_listen_shared_access_key" {
-  value = azurerm_servicebus_namespace_authorization_rule.servicebus_authorization_rule.secondary_key
+  value = azurerm_servicebus_namespace_authorization_rule.send_listen_auth_rule.secondary_key
 }
 
 # sku type
