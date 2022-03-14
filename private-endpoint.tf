6865773c431354dd8fcd78e341510702df4a3e2e
msl8r <matt.slater@hmcts.net>
2022-03-14 16:49:43 +0000

    DTSPO-6371 - revert changes made for subnet list

--- private-endpoint.tf
+++ private-endpoint.tf
@@ -5,7 +5,7 @@ resource "azurerm_private_endpoint" "this" {
   name                = "${var.name}-endpoint"
   location            = var.location
   resource_group_name = var.resource_group_name
-  subnet_id           = data.azurerm_subnet.this.id
+  subnet_id           = var.subnet_id
 
   private_service_connection {
     name                           = "${var.name}-endpoint-namespace"
