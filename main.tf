9f11d3e6620d55e5c00da55a122009e5dd3269d5
msl8r <72858053+msl8r@users.noreply.github.com>
2022-03-16 14:50:16 +0000

    DTSPO-6371 - fix sku condition

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_servicebus_namespace" "servicebus_namespace" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = var.enable_private_endpoint != "" ? "Premium" : var.sku
+  sku                 = var.enable_private_endpoint ? "Premium" : var.sku
   zone_redundant      = var.zoneRedundant
   tags                = var.common_tags
   capacity            = var.capacity
