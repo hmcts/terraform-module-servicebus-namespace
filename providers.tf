50dc2e05509c37e46c4bd46496140cb442df13b1
msl8r <matt.slater@hmcts.net>
2022-03-29 19:32:32 +0100

    DTSPO-6371 - remove sub id

--- providers.tf
+++ providers.tf
@@ -4,5 +4,5 @@ provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "private-endpoint-subnet"
-  subscription_id            = var.project == "sds" ? "a8140a9e-f1b0-481f-a4de-09e2ee23f7ab" : var.aks_subscription_id
+  subscription_id            = var.project == "sds" ? "" : var.aks_subscription_id
 }
