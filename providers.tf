21763410879a184dd30030adc02db03f1e5aa5b5
msl8r <matt.slater@hmcts.net>
2022-03-29 17:59:38 +0100

    DTSPO-6371 - try and fix provider config for sds

--- providers.tf
+++ providers.tf
@@ -1,6 +1,8 @@
+data "azurerm_client_config" "current" {
+}
 provider "azurerm" {
   features {}
   skip_provider_registration = true
   alias                      = "private-endpoint-subnet"
-  subscription_id            = var.aks_subscription_id
+  subscription_id            = var.project == "sds" ? "" : var.aks_subscription_id
 }
