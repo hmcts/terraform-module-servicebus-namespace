c2d515957ae669b915f3f4992be6d49ba4f97964
msl8r <matt.slater@hmcts.net>
2022-03-29 17:14:44 +0100

    DTSPO-6371 - fix provider

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
-  alias           = "private-endpoint-subnet"
-  subscription_id = var.aks_subscription_id
+  skip_provider_registration = true
+  alias                      = "private-endpoint-subnet"
+  subscription_id            = var.aks_subscription_id
 }
