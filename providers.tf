b7fc206258bf7667e496049bd6db7546eff8b88c
msl8r <72858053+msl8r@users.noreply.github.com>
2022-03-30 09:56:21 +0100

    DTSPO-6371 - deploy private endpoint to vnet rg (#22)

---
+++ providers.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "private-endpoint-subnet"
+  subscription_id            = var.private_endpoint_subscription_id
+}
