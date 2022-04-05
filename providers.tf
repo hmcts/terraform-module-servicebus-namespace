9d7c984c6e2cb5655b2876c9e179bdaf8b8fa2c7
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-05 16:07:39 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -3,6 +3,6 @@
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-  alias                      = "private-endpoint-subnet"
+  alias                      = "private-endpoint"
   subscription_id            = var.private_endpoint_subscription_id
 }
