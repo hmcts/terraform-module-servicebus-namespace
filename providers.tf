348af10cda3d9828c661e2b2424e6b7035a90c15
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-05 16:36:48 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,9 @@
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  alias                      = "private-endpoint-subnet"
-  subscription_id            = var.private_endpoint_subscription_id
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0.0"
+      configuration_aliases = [ azurerm.private-endpoint ]
+    }
+  }
 }
