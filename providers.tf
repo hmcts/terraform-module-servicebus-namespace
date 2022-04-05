85c0d4449d18a2d7d871d5e629f6ccbaef578391
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-05 16:14:52 +0100

    Delete providers.tf

--- providers.tf
+++
@@ -1,8 +0,0 @@
-
-   
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  alias                      = "private-endpoint"
-  subscription_id            = var.private_endpoint_subscription_id
-}
