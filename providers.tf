1452758ca6288185a61709dc1982fbe5d949ae56
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-07 22:46:48 +0100

    fmt

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.0.0"
+      source                = "hashicorp/azurerm"
+      version               = ">= 2.0.0"
       configuration_aliases = [ azurerm.private_endpoint ]
     }
   }
