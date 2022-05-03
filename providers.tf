f8c4b05294cbbb2af95ffc66d2d72633e411614a
msl8r <matt.slater@hmcts.net>
2022-05-03 14:42:08 +0100

    DTSPO-6371 - up the min version of azurerm tf fmt

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = ">= 2.0.0"
-      configuration_aliases = [ azurerm.private_endpoint ]
+      version               = ">= 3.0.0"
+      configuration_aliases = [azurerm.private_endpoint]
     }
   }
 }
