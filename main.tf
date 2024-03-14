14e7ea8196377abee6e3dd78fe1715dc2585cde6
Alex Bance <Alex.Bance@justice.gov.uk>
2024-03-14 14:59:53 +0000

    feat: inline network rulesets

--- main.tf
+++ main.tf
@@ -14,6 +14,11 @@ resource "azurerm_servicebus_namespace" "servicebus_namespace" {
   tags                         = var.common_tags
   capacity                     = local.capacity
   premium_messaging_partitions = local.premium_messaging_partitions
+
+  network_rule_set {
+    default_action                = "Allow"
+    public_network_access_enabled = var.enable_public_access
+  }
 }
 
 resource "azurerm_servicebus_namespace_authorization_rule" "send_listen_auth_rule" {
