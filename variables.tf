dea9e0da3a76d9f58ab1c1761787be2ca7a60ebf
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-05-05 16:10:47 +0100

    add new variable

--- variables.tf
+++ variables.tf
@@ -40,8 +40,15 @@ variable "capacity" {
 
 variable "enable_public_access" {
   type        = bool
-  default     = true
+  default     = false
   description = "Enable public access (should only be enabled for a migration when using the Premium SKU and a private endpoint connection)"
+}
+
+# enable_private_access is separate to enable_public_access to ensure backwards compatibility with existing module calls.
+variable "enable_private_access" {
+  type        = bool
+  default     = false
+  description = "Disable public access in favour of private endpoint only"
 }
 
 variable "enable_private_endpoint" {
